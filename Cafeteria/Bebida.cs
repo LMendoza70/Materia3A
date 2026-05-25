b9ba809b1e4c6cc929b6a85680976fa2b624d25f
MTI.LuisAlbertoMendoza <luis.mendoza@uthh.edu.mx>
2026-05-25 13:57:06 -0600

    Aplicacion de polimofismo y herencia
    Añade clases Bebida y BebidaCaliente con lógica básica
    
    Se implementa la clase Bebida con propiedades, constructores y métodos para preparar y aplicar descuentos. Se añade la clase BebidaCaliente que hereda de Bebida, incorpora la propiedad Temperatura y redefine el método Preparar.

---
+++ Cafeteria/Bebida.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Materia3A.Cafeteria
+{
+    internal class Bebida
+    {
+        protected float Precio;
+        protected string Nombre;
+        protected string Tamaño;
+
+        public float precio
+        {
+            get { return Precio; }
+            set
+            {
+                if (value > 0)
+                {
+                    Precio = value;
+                }
+                else
+                {
+                    Precio = -1;
+                }
+            }
+        }
+
+        public string tamaño{
+            get { return Tamaño; }
+            set { Tamaño = value; }
+        }
+
+        public string nombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+
+        public Bebida()
+        {
+            Nombre = "";
+            Tamaño = "";
+            Precio = 0;
+        }
+
+        public Bebida(string _nombre, string _tamaño, float _precio)
+        {
+            this.Nombre = _nombre;
+            Tamaño = _tamaño;
+            Precio = _precio;
+        }
+
+        public virtual string Preparar()
+        {
+            return "Estamos preparando un : "+Nombre + " De tamño : "+Tamaño;
+        }
+
+        public void Descuento(float descuento)
+        {
+            Precio = Precio * (1 - (descuento / 100));
+        }
+    }
+}
---
+++ Cafeteria/BebidaCaliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Materia3A.Cafeteria
+{
+    internal class BebidaCaliente:Bebida
+    {
+        private float Temperatura;
+
+        public float temperatura
+        {
+            get {  return Temperatura; }
+            set { Temperatura = value; }
+        }
+
+        public BebidaCaliente(string _nombre,string _tamaño, float _temperatura, float _precio):base(_nombre,_tamaño,_precio)
+        {
+            Temperatura = _temperatura;
+        }
+
+        public override string Preparar()
+        {
+            return "Estamos preparando un : "+Nombre+" Caliente  a una temperatura de : "+Temperatura + " tamaño : "+Tamaño;
+        }
+    }
+}
